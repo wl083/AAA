52840d67865941415b6b68ea4949a724655f9ce1
吴磊 <1091456146@qq.cm>
2018-03-18 20:28:47 +0800

    18

--- app/src/main/java/com/example/administrator/aaa/Test.java
+++ app/src/main/java/com/example/administrator/aaa/Test.java
@@ -3,7 +3,7 @@ package com.example.administrator.aaa;
 /**
  * Created by wl
  * On 2018/2/23
- * Describe:
+ * Describe:18
  */
 
 public class Test {
---
+++ app/src/main/java/com/example/administrator/aaa/Tre.java
@@ -0,0 +1,7 @@
+package com.example.administrator.aaa;
+
+/**
+ * Created by lei on 2018/3/18.
+ */
+public class Tre {
+}
